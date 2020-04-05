112ab7ec63f3e9b0cb39232509d7c4a64b9a870e
chachachacharlirae <61812759+chachachacharlirae@users.noreply.github.com>
2020-04-05 18:38:35 -0500

    Hard upload
    terminal says tree is clean but git hub has not been updating my commits.

--- Develop/script.js
+++ Develop/script.js
@@ -1,69 +1,119 @@
-// Assignment code here
-const key_string = {
-  lowercase: 'abcdefghijklmnopqrstuvwxyz',
-  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  number: '1234567890';
-  symbol: '!"!#$%^&*()=,.-:;' ?<>';,
 
-  //generate password
-function generatePassword(){
-  var passwordCharSet ="";
-}
+let lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
 
-    //Get references to generate element var generatePassword()
-    {
-    var passwordCharSet ="";
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
-    var length =parseInt(window.prompt("Enter a number from 8 to 128 for password length."))
-    if (length){
+let special = "*^&%#$@!()-_+=?/\[]{}<>".split("");
 
-    };
-    console.log(length);
-    var lowercase =window.confirm ("Would you like to use lowercase letters?");
-    if (lowercase==true)
+let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    var uppercase = window.confirm ("Would you lile to use uppercase letters?");
-    if (uppercase==true)
-    {passwordCharset = +key_strings.uppercase;
-    };
-    var symbols=widow.confirm("would you like to use symbols?");
-    if (symbols) {
-      passwordCharSet += key_strings.symbol;
-    };
-    var numbers = window.comfirm ("Would you like to use numbers?");
-  };
-  if (numbers){
-      passwordCharSet += key_strings.number;
-  };
-  var password = "";
-  for (let i=); i < length, i++) {
-      password += passwordCharset[MathFloor{Math.random() * passwordCharSet.length)]
-  }
-  return password;
 
-  }
+//characters 
+
+function promptForType(question) {
+  let response = prompt(question).toLowerCase();
+  do {
+    if (response === "yes") {
+      return true;
     }
 
+    else if (response === "no") {
+      return false;
+    }
 
-      passwordCharset += key_strings.lowercase;
-    };
-    var upper
-  }
+    else {
+      response = prompt("Please try again. " + question).toLowerCase();
+    }
 
-};
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  } while (true);
 
 }
 
 
-// Add event listener to generate button
+//length
+
+function promptForLength() {
+  let userResponse = prompt("Number of characters? (8 to 128 characters please)");
+  let numberResponse = Number(userResponse);
+
+  while (isNaN(numberResponse) || numberResponse < 8 || numberResponse > 128) {
+    if (isNaN(numberResponse)) {
+      userResponse = prompt("Please enter a number.");
+    }
+
+    else if (numberResponse > 128) {
+      userResponse = prompt("Exceeded character limit. Please enter less than 128.");
+    }
+
+    else if (numberResponse < 8) {
+      userResponse = prompt("More characters needed. Please enter more than 8.");
+    }
+
+    numberResponse = Number(userResponse);
+  }
+
+  return numberResponse;
+
+}
+
+
+
+function generatePassword() {
+  let useSpecial = false;
+  let length = promptForLength();
+  let useUpperCase = false;
+  let useLowerCase = false;
+  let useNumbers = false;
+
+
+
+  do {
+    useSpecial = promptForType("Would you like to use symbols? (yes or no)");
+    useNumbers = promptForType("Would you like to use numbers? (yes or no)");
+    useUpperCase = promptForType("Would you like to use upper case letters? (yes or no)");
+    useLowerCase = promptForType("Would  you like to use lower case letters? (yes or no)");
+
+
+    if (!useLowerCase && !useNumbers && !useUpperCase && !useSpecial) {
+
+      alert("Please choose a character type.");
+    }
+  } while (!useLowerCase && !useNumbers && !useUpperCase && !useSpecial);
+
+  let passArray = [];
+
+
+  if (useSpecial) {
+    passArray.splice(0, 0, ...special);
+  }
+
+  if (useLowerCase) { passArray.splice(0, 0, ...lowerCase); }
+
+  if (useUpperCase) { passArray.splice(0, 0, ...upperCase); }
+
+  if (useNumbers) { passArray.splice(0, 0, ...numbers); }
+
+  let password = "";
+  for (i = 0; i <= length; i++) {
+    let randomNum = Math.floor(Math.random() * passArray.length);
+    password += passArray[randomNum];
+  }
+
+  return password;
+}
+
+
+
+
+//write password function
+
+function writePassword() {
+  var passwordText = document.querySelector("#password");
+  passwordText.value = " ";
+  var password = generatePassword();
+  passwordText.value = password;
+}
+
+var generateBtn = document.querySelector("#generate");
+
 generateBtn.addEventListener("click", writePassword);
